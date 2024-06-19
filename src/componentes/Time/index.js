9b149447e6306944bcc2f8cce7ee839019637f07
Lucas <lucasv.cornelius@gmail.com>
2024-06-19 15:57:55 -0300

    making form interaction

---
+++ src/componentes/Time/index.js
@@ -0,0 +1,12 @@
+import './Time.css';
+
+const Time = (props) => {
+    return (
+        <section className='card'>
+            <h3>{props.nome}</h3>
+            <div></div>
+        </section>
+    );
+};
+
+export default Time;
